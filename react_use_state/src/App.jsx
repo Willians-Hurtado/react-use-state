06200d38431d54e29640c8a39b2555f4daaf9059
Willians-Hurtado <willianshurtado5@gmail.com>
2025-03-25 16:19:21 +0100

    first commit

---
+++ react_use_state/src/App.jsx
@@ -0,0 +1,20 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header'
+import Main from './components/Main'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+
+  return (
+    <>
+      <Header />
+
+
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ react_use_state/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+
+    return (
+        <header>
+            <div className="title">
+                <h1>Learn Web development</h1>
+            </div>
+
+        </header>
+    )
+}
